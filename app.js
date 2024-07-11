3a59a9af387aa551fcdf61ec08d1980b2fccd2ae
Pablo Teodoro <132463199+pabloteodoro@users.noreply.github.com>
2024-07-11 18:55:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+
+
+const PORT = 8001;
+
+app.listen(PORT, function() {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// db connection
+
+db
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Server Okay Baby');
+});
+
+
+
+
