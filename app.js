36ee42fdafe93cd363abb8dc502340666b8969ae
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-07-25 17:51:08 -0300

    update sintaxe
    switch all sintexes

--- app.js
+++ app.js
@@ -1,62 +1,78 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const app = express();
-const Job = require('./models/Job');
-const path = require('path');
-const db = require('./db/connection');
+const express    = require('express');
+const { engine } = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
+const PORT = 3000;
 
-
-const port = process.env.PORT || 4002;
-
-app.listen(port,() => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, function() {
+  console.log(`O Express está rodando na porta ${PORT}`);
 });
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// handlebars
+// handle bars
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs.engine({ defaultLayout: "main" }));
+app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // db connection
-
 db
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso");
+  })
+  .catch(err => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
 
 // routes
 app.get('/', (req, res) => {
 
-    let search = req.query.job;
-    if(!search) {
-        Job.findAll({order: [
-            ['createdAt', 'DESC']
-        ]})
-       .then(jobs => {
-    
-        res.render('index', { 
-            jobs 
-        });
-    
-    });
-    }
+  let search = req.query.job;
+  let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
 
+  if(!search) {
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+  
+      res.render('index', {
+        jobs
+      });
+  
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      console.log(search);
+      console.log(search);
+  
+      res.render('index', {
+        jobs, search
+      });
+  
+    })
+    .catch(err => console.log(err));
+  }
+
+  
 });
 
-
+// jobs routes
 app.use('/jobs', require('./routes/jobs'));
-
-
